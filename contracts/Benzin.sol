873fdf7baeb16f05fa1cc193c49a1afda23bcd2d
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-08-15 16:33:32 -0400

    contract Benzin.sol  added

---
+++ contracts/Benzin.sol
@@ -0,0 +1,138 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+contract Station {
+    
+    struct Video {
+        string ipfsHash;
+        string title;
+        uint256 timeStamp;
+        uint256 size;
+        uint256 tipjar;
+        uint256 views;
+        address payable publisher;
+    }
+    
+    struct UserVideoList {
+        uint256 length; // total videos
+        uint256 tipjar;
+        mapping(string => Video) videos; // IPFS hash
+    }
+    
+    //mapping(address => uint256) jarShelf; // having track of how many tips received by the publisher
+    //The jarShelf mapping was removed because I think it should be about videos. Later we can get all the videos.
+    
+    mapping(address => UserVideoList) public publishers; // listing all videos of each publisher
+    
+    mapping(string => Video) videos; // IPFS HASH
+    
+    modifier videoExists(string memory _ipfsHash) {
+        require(videos[_ipfsHash].publisher != address(0) , "Videos does not exist");
+        _;
+    }
+    
+    function DB_insert(
+        string memory _ipfsHash,
+        string memory _title,
+        uint256 _timestamp,
+        uint256 _size,
+        uint256 _tipjar,
+        uint256 _views,
+        address payable _publisher
+    ) public {
+        Video memory video = Video( _ipfsHash, _title, _timestamp, _size, _tipjar, _views, _publisher);
+        videos[_ipfsHash] = video;
+        //increasing the length of videos
+        publishers[_publisher].length++;
+        publishers[_publisher].videos[_ipfsHash] = video;
+    }
+    
+    function DB_update(
+        string memory _ipfsHash,
+        string memory _title,
+        uint256 _timestamp,
+        uint256 _size,
+        uint256 _tipjar,
+        uint256 _views,
+        address payable _publisher) public videoExists(_ipfsHash)
+    {
+        
+        Video memory video = Video( _ipfsHash, _title, _timestamp, _size, _tipjar, _views, _publisher);
+        videos[_ipfsHash] = video;
+        publishers[_publisher].videos[_ipfsHash] = video;
+    }
+    
+    function DB_get(string memory _ipfsHash) public view videoExists(_ipfsHash) returns(Video memory) 
+    {
+        Video storage video = videos[_ipfsHash];
+        return video;
+    }
+    
+    function DB_delete(string memory _ipfsHash, address _publisher) public videoExists(_ipfsHash)
+    {
+        
+        delete videos[_ipfsHash];
+        delete publishers[_publisher];
+    }
+    
+    function DB_tipMade(
+        uint256 _amount,
+        string memory _ipfsHash,
+        address payable _publisher) public videoExists(_ipfsHash) 
+    {
+        require(_amount > 0, "DB_err : 0 tip");
+        require(videos[_ipfsHash].publisher != address(0) , "Videos does not exist");
+        
+        videos[_ipfsHash].tipjar ++;
+        publishers[_publisher].videos[_ipfsHash].tipjar ++;
+        publishers[_publisher].tipjar ++;
+    }
+}
+
+
+
+contract Benzin is Station {
+    address private owner;
+    
+    event newVideo(string ipfsHash, string message);
+    event tipMade(uint256 amount, string ipfsHash, address sender);
+    
+    constructor() public{
+        owner = msg.sender;
+    }
+    
+    modifier isOwner {
+        require(msg.sender == owner);
+       _; 
+    }
+    
+    function tip(address payable _publisher, string memory _ipfsHash) public payable {
+        require(msg.value > 0, '0-tip err.');
+        require(_publisher != address(0), 'invalid address');
+        DB_tipMade(msg.value, _ipfsHash, _publisher);
+        
+        emit tipMade(msg.value, _ipfsHash, msg.sender);
+    }
+    
+    function addVideo(
+        string memory _ipfsHash,
+        string memory _title,
+        uint256 _timestamp,
+        uint256 _size,
+        address payable _publisher)
+        public
+    {
+        require(msg.sender == _publisher, 'you must send the video using your own address');
+        require(_size > 0 && _timestamp < now);
+        DB_insert(_ipfsHash, _title, _timestamp, _size, 0, 0, _publisher);
+        emit newVideo(_ipfsHash, "Video added successfully");
+    }
+    
+    function getVideo(string memory _ipfsHash) public view videoExists(_ipfsHash) returns(Video memory) {
+        return DB_get(_ipfsHash);
+    }
+    
+    // function editVide() {}
+    // function deleteVideo(){}
+    
+}
